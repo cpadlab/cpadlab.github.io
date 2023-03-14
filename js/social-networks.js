ef9335e9af97c97274a81c7bc48186503040719d
Wual1014 <cpadlab@gmail.com>
2023-03-14 02:33:38 +0100

    14/03.06

--- js/social-networks.js
+++ js/social-networks.js
@@ -1,12 +1,12 @@
 if (document.title === "Github") {
-    window.location.href = "https://github.com/14wual", "_BLANK";
+    window.location.href = "https://github.com/14wual";
 }
 else if (document.title === "Twitter") {
-    window.location.href = "https://twitter.com/codewual", "_BLANK";
+    window.location.href = "https://twitter.com/codewual";
 }
 else if (document.title === "YouTube") {
-    window.location.href = "https://youtube.com/@wual", "_BLANK";
+    window.location.href = "https://youtube.com/@wual";
 }
 else if (document.title === "LinkedIn") {
-    window.location.href = "https://www.linkedin.com/in/cpadilla10", "_BLANK";
+    window.location.href = "https://www.linkedin.com/in/cpadilla10";
 }
