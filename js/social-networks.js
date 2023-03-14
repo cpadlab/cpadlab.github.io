a96739eeedaf25ec3b0cb56cfc1f0435b7144b39
Wual1014 <cpadlab@gmail.com>
2023-03-14 02:17:06 +0100

    14/03.03

---
+++ js/social-networks.js
@@ -0,0 +1,13 @@
+if (document.title === "Github") {
+    window.open("https://github.com/14wual", "_BLANK");
+}
+else if (document.title === "Twitter") {
+    window.open("https://twitter.com/codewual", "_BLANK");
+}
+else if (document.title === "YouTube") {
+    window.open("https://youtube.com/@wual", "_BLANK");
+}
+else if (document.title === "LinkedIn") {
+    window.open("https://www.linkedin.com/in/cpadilla10", "_BLANK");
+}
+window.location.href = "http://14wual.github.io/index.html"  
