73f4fbc8d4aa7301f86e1e0fd4288640117b2e95
14wual <cpadlab@gmail.com>
2023-05-30 16:01:32 +0200

    Redesign Fase 1

---
+++ images/color-palete.py
@@ -0,0 +1,38 @@
+
+##################
+###COLOR.PALETE###
+##################
+
+class getColors:
+
+    def __init__(self) -> None:
+    
+        colors = Colors()
+
+        neon = colors.neon
+        basic = colors.basic
+
+        print("Neon: " , neon , "\nBasics: " , basic)
+
+class Colors:
+
+    def __init__(self) -> None:
+        
+        self.neon = {
+            'blue': '#01c4e7',
+            'pink': '#e701c4',
+            'yellow': '#c4e701',
+        }
+
+        self.basic = {
+            'bg': '#1c1c1c',
+            'white-text': '#f4f4f4',
+            'grey-text': '#616161',
+            'other': {
+                '4d4d4d',
+                '393939',
+            }
+        }
+
+if __name__ == '__main__':
+    getColors()
