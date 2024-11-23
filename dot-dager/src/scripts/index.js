93744adae92f43d6a6e87334abc4d952b8d1d66f
Carlos Padilla <105047274+cpadlab@users.noreply.github.com>
2024-11-23 17:45:19 +0100

    DotDager Update
    Lang ES & Music Section

--- dot-dager/src/scripts/index.js
+++ dot-dager/src/scripts/index.js
@@ -35,11 +35,48 @@ function createProjectCard(project) {
     return `<a class="gc-card" target="_blank" href="${project.url}"><h3>${project.name}</h3><p>${project.desc}</p><div class="gcc-labels"><div><time datetime="${project.date}">${new Date(project.date).toLocaleDateString('en-US', {month: 'short',day: 'numeric'})}</time></div><div><span>${project.lang}</span></div><div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/></svg><span>${project.stars}</span></div></div></a>`
 }
 
+function createMusicCard(song) {
+    return `
+                <a href="${song.song_url}" target="_blank" class="mc-songs-card">
+                    <div class="mc-songs-card-image">
+                        <div class="mc-songs-card-hover">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c-7.6 4.2-12.3 12.3-12.3 20.9l0 176c0 8.7 4.7 16.7 12.3 20.9s16.8 4.1 24.3-.5l144-88c7.1-4.4 11.5-12.1 11.5-20.5s-4.4-16.1-11.5-20.5l-144-88c-7.4-4.5-16.7-4.7-24.3-.5z"/>
+                            </svg>
+                        </div>
+                        <img src="${song.cover_page}" alt="${song.song_name} Cover Page" loading="lazy">
+                    </div>
+                    <h3>${song.song_name}</h3>
+                </a>
+            `;
+}
+
 $(document).ready(function () {
 
-    $.getJSON('src/content/projects.json', function (projects) {
+    $.getJSON('/dot-dager/src/content/projects.json', function (projects) {
         $.each(projects, function (index, project) {
             $('.github-container').append($(createProjectCard(project)));
+        });
+    });
+
+    $.getJSON('/dot-dager/src/content/music.json', function(data) {
+        
+        $.each(data, function(index, song) {
+            $('.mc-songs').append($(createMusicCard(song)));
+        });
+
+        $('.mc-songs').slick({
+            infinite: true,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: false,
+            variableWidth: true,
+            adaptiveHeight: false,
+            lazyLoad: "progressive",
+            mobileFirst: true,
+            pauseOnFocus: false,
+            pauseOnHover: false
         });
     });
 
