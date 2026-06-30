05931368cfa223d761be53db8b9787643a37f296
Carlos Padilla <105047274+cpadlab@users.noreply.github.com>
2026-06-30 14:55:55 +0200

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import { HomeAboutSection } from "@/views/home/about";
 import { HomeViewEffect } from "@/views/home/view";
-import { HomeBlogSection } from "@/views/home/blog";
 import { StickyFooter } from "@/views/sticky-footer";
 import { getAllPosts } from "@/lib/blog";
 
 export default function Page() {
     
-    const posts = getAllPosts();
-
     return (
         <main className="bg-black text-white min-h-dvh">
             <HomeViewEffect />
